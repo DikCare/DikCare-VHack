3341ea7aca29405d1a87577254c3661d977ceae9
Jialegoh <cteh60112@gmail.com>
2025-03-27 13:42:59 +0800

    push

--- PatientUserMobileApplication/vite.config.js
+++ PatientUserMobileApplication/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DikCare-VHack/PatientUserMobileApplication/', // Replace 'dikcare' with your repository name
+  base: "/DikCare-VHack/PatientUserMobileApplication/",
+  build: {
+    outDir: "../docs/PatientUserMobileApplication/dist", // Ensure the build output is in docs/
+  },
 })
